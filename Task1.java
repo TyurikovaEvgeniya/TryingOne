2434f75055944f29d1964df6401289f2bb17281d
pal548 <gopa3000@bk.ru>
2017-01-30 00:26:06 +0300

    Hello в мой репозиторий

---
+++ Task1.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Евгения on 30.01.2017.
+ */
+class Task1 {
+    public static void main(String[] args) { //метод main- точка входа в программу/
+        System.out.println("Hello World"); //System.out.println осуществляет вывод строки в консоль
+    }
+}
