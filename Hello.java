91854ba8cbca7470006f24ca9c7b259c14e6534c
TyurikovaEvgeniya <kirra.sekirra@gmail.com>
2017-02-06 00:51:59 +0300

    Переименование Hello из-за пересечения названий файла с задачей Task1.

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 /**
  * Created by Евгения on 30.01.2017.
  */
-class Task1 {
+class Hello {
     public static void main(String[] args) { //метод main- точка входа в программу/
         System.out.println("Hello World"); //System.out.println осуществляет вывод строки в консоль
     }
